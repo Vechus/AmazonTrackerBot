a15493ea8ac22c2e30de302f9d793204dfe18e3e
Vechus <luca1.vecchio@mail.polimi.it>
2018-04-17 22:03:08 +0200

    Aggiunte cose piene di bug
    funzione test per testare la funzione principale

--- amazonRequest.py
+++ amazonRequest.py
@@ -2,13 +2,22 @@ import requests
 import logging
 from bs4 import BeautifulSoup
 
-def requestPriceUrl(chat_id, url):
-    r = requests.get(url)
+USERREQUEST = 25
+logging.addLevelName(USERREQUEST, 'USER REQUEST: ')
+logging.basicConfig(filename='testing.log', filemode='w+', level=logging.DEBUG)
+
+def requestPriceUrl(user, url):
+    r = requests.get(url.split("/test ")[1])
     soup = BeautifulSoup(r.text, "html.parser")
     product_name = soup.find("span", {"id": "productTitle"})
     product_name_text = " ".join(product_name.text.split())
+    logging.log(USERREQUEST, "from: " + user + ": product: " + product_name_text)
     # TODO-vechus implement all cases
-    price = soup.find("span", {"id": "priceblock_saleprice"})
-    price_text = prezzo.text
-
-
+    # saleprice case
+    try:
+        print("Product found for ", user, ": ", str(product))
+        price = soup.find("span", {"id": "priceblock_ourprice"})
+        product = [product_name_text, str(price)]
+        return product
+    except:
+        print("Error. The product is NULL")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import telebot
 import logging
 import simplejson
+import amazonRequest
 
 FEEDBACK = 12
 logging.addLevelName(FEEDBACK, 'FEEDBACK')
 logging.basicConfig(filename='testing.log', filemode='w+', level=logging.DEBUG)
 
 bot = telebot.TeleBot("577135328:AAEBSQdPZE3UO4aUqt_pAWrrbBaGTST1DRM")
+print("Starting...")
 
 ## Start / Help
 @bot.message_handler(commands=['start','help'])
@@ -18,6 +20,18 @@ def sendFeedback(message):
     logging.log(FEEDBACK, "from: " + str(message.chat.username) + ": " + message.text)
     bot.reply_to(message, "Feedback sent!")
 
+@bot.message_handler(commands=['test'])
+def testReq(message):
+    if message.text.find('amazon.it', 0) == -1:
+        bot.reply_to(message, "You didn't input an amazon.it url... Stupid fucking tester.")
+    else:
+        product_found = {}
+        product_found = amazonRequest.requestPriceUrl(str(message.chat.username), message.text)
+        try:
+            bot.reply_to(message, "Found a product, it is: " + product_found[0] + ", price: " + product_found[1] + "€.")
+        except:
+            bot.reply_to(message, "The product has been found, but the dev is too lazy to implement new cases, I'm sorry. but hey, at least I found it, here it is!")
+
 
 ## Not implemented / not a command / bugged
 @bot.message_handler(func=lambda m: True)
@@ -25,4 +39,5 @@ def echo_all(message):
 	bot.reply_to(message, message.text + " is not a command.")
 
 ##### START BOT #####
+print("Bot started.")
 bot.polling()
